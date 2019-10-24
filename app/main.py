033433821eec4f29b58b6e6648934c576561138c
stevenlin <stevenlin@cybersoft4u.com>
2019-10-24 20:51:34 +0800

    build basic architecture of jirapoker

---
+++ app/main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import requests
+import json
+from flask import jsonify
+from flask import Flask
+from flask import request
+
+from werkzeug.exceptions import HTTPException
+
+import os
+import logging.config
+
+_PATH = os.path.dirname(os.path.abspath(__file__))
+_PATH = os.path.join(_PATH, 'logging.ini')
+DEFAULT_LOG_CONFIG = os.path.abspath(_PATH)
+
+logging.config.fileConfig(DEFAULT_LOG_CONFIG)
+logger = logging.getLogger('flask')
+
+app = Flask(__name__)
+
+
+@app.errorhandler(Exception)
+def handle_error(e):
+    code = 500
+    if isinstance(e, HTTPException):
+        code = e.code
+    logger.error('%s', str(e))
+    return jsonify(error=str(e)), code
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
