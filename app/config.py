649c297143d6a77b7653c7e2e561af611245f26e
stevenlin <stevenlin@cybersoft4u.com>
2019-12-26 22:24:37 +0800

    add jirapoker and mongodb to docker-compose.yml

--- app/config.py
+++ app/config.py
@@ -5,9 +5,10 @@ DEFAULT_LOG_CONFIG = os.path.join(_PATH, 'logging.ini')
 
 # Jira auth
 JIRA_URL = 'https://cybersoft4u.atlassian.net'
-JIRA_USER = 'xxx'
-JIRA_TOKEN = 'xxx'
+JIRA_USER = os.environ['JIRA_USER']
+JIRA_TOKEN = os.environ['JIRA_TOKEN']
 
 # Mongo db connection
-MONGO_URI = 'mongodb://localhost:27017'
-DATABASE_NAME = 'jirapoker'
+MONGO_HOST_NAME = os.environ['MONGO_HOST_NAME']
+MONGO_URI = 'mongodb://{hostname}:27017'.format(hostname=MONGO_HOST_NAME)
+DATABASE_NAME = os.environ['MONGO_DB_NAME']
