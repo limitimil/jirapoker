c5393b36251f58021df35143760c8301aa2766a8
stevenlin <stevenlin@cybersoft4u.com>
2019-12-27 22:05:48 +0800

    modify docker-compose and Dockerfile for nginx.conf

---
+++ app/entrypoint.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env bash
+set -e
+
+/uwsgi-nginx-entrypoint.sh
+
+
+# Get the Backend host for Nginx, default to localhost
+BACKEND_HOST=${BACKEND_HOST:-'localhost'}
+
+# Get the Backend port for Nginx, default to 5000
+BACKEND_PORT=${BACKEND_PORT:-5000}
+
+# Get the listen port for Nginx, default to 80
+USE_LISTEN_PORT=${LISTEN_PORT:-80}
+
+# Get the listen port for Nginx, default to 80
+USE_LISTEN_PORT=${LISTEN_PORT:-80}
+
+if [ -f /app/nginx.conf ]; then
+    cp /app/nginx.conf /etc/nginx/nginx.conf
+else
+    content_server='server {\n'
+    content_server=$content_server'    listen ${USE_LISTEN_PORT};\n'
+    content_server=$content_server'    client_body_timeout 15s;\n'
+    content_server=$content_server'    client_header_timeout 15s;\n'
+    content_server=$content_server'    location @app {\n'
+    content_server=$content_server'        include uwsgi_params;\n'
+    content_server=$content_server'        uwsgi_pass unix:///tmp/uwsgi.sock;\n'
+    content_server=$content_server'    }\n'
+    content_server=$content_server'    location /socket.io {\n'
+    content_server=$content_server'        include proxy_params;\n'
+    content_server=$content_server'        proxy_http_version 1.1;\n'
+    content_server=$content_server'        proxy_buffering off;\n'
+    content_server=$content_server'        proxy_set_header Upgrade $http_upgrade;\n'
+    content_server=$content_server'        proxy_set_header Connection "Upgrade";\n'
+    content_server=$content_server'        proxy_pass http://${BACKEND_HOST}:${BACKEND_PORT}/socket.io;\n'
+    content_server=$content_server'    }\n'
+
+    # Save generated server /etc/nginx/conf.d/nginx.conf
+    printf "$content_server" > /etc/nginx/conf.d/nginx.conf
+fi
+
+exec "$@"
--- app/main.py
+++ app/main.py
@@ -72,5 +72,5 @@ def connected():
 
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=80, debug=True)
+    socketio.run(app, host='0.0.0.0', debug=True)
     # app.run(host='0.0.0.0', port=80)
